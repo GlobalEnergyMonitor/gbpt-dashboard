36985774da54aaed32d15a2da9de49346fdb777a
gem-tah <taylor.higgins@globalenergymonitor.org>
2026-09-16 17:17:27 -0500

    rewired to pull from 2026 data not 2025

--- public/index.js
+++ public/index.js
@@ -27,12 +27,12 @@ async function getData() {
         .then(() => {
             const dataURLS = [];
             config.dashboard.flourish_ids.forEach(id => {
-                dataURLS.push(`./assets/data_2025/${config.charts[id].dataset}.json`);
+                dataURLS.push(`./assets/data_2026/${config.charts[id].dataset}.json`);
                 config.datasets[id] = [];
             })
             if (config.dashboard.tickers) {
                 dataURLS.push('https://public.flourish.studio/visualisation/16565310/visualisation.json') // this assumes we want the same template for all tickers
-                dataURLS.push(`./assets/data_2025/${config.dashboard.ticker_data}.json`)
+                dataURLS.push(`./assets/data_2026/${config.dashboard.ticker_data}.json`)
                 config.datasets.ticker = {};
             }
             const fetches = [];
